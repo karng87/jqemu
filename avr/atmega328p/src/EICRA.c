753ad4bfceb8294ddf390c220a79b9ce2bbfdbcb
karng87 <karng87@gmail.com>
2022-10-15 02:57:32 +0900

    asus

--- avr/atmega328p/src/EICRA.c
+++ avr/atmega328p/src/EICRA.c
@@ -1,45 +1,52 @@
+#include<avr/io.h>
+#include <avr/interrupt.h>
 #define F_CPU 16000000UL
-
-#define __INTR_ATTRS __used__, __externally_visible__
-void __vector_1(void) __attribute__ ((__signal__, __used__, __externally_visible__));
-void __vector_1(void){
-    *(volatile unsigned char*)0x25 |= (1<<0); // PORTB(0): high
-    for(unsigned long l=0; l<0xFFFF00; l++);
-}
-
-void __vector_2(void) __attribute__ ((__signal__, __used__, __externally_visible__));
-void __vector_2(void){
-    *(volatile unsigned char*)0x25 |= (1<<1); // PORTB(1): high
-    for(unsigned long l=0; l<0xFFFF00; l++);
-}
-
-/*
-ISR(__vector_2){
-    *(volatile unsigned char*)0x25 ^= 0x02;
-    for(unsigned long l=0; l<100000; l++);
-}
-*/
-
 int main(){
 
-    // DDRB(0x24): ( PB0: PB1: PB2: PB3: ) <- OUTPUT_MOD(1):
-    *(volatile unsigned char*)0x24 |= (1<<0 | 1<<1 | 1<<2 | 1<<3);
+        // DDRB(0x24): ( PB0: PB1: PB2: PB3: ) <- OUTPUT_MOD(1):
+    *(volatile unsigned char*)0x24 = 0b00001111;
 
-    // DDRD(0x2A): ( PD2: PD3: ) <- INPUT_MOD(0):
+        // DDRD(0x2A): ( PD2: PD3: ) <- INPUT_MOD(0):
     *(volatile unsigned char*)0x2A &= ~(1<<2 | 1<<3);
-    // PORTD(0x2B): ( PD2: PD3: ) <- PULL_UP(1):
-    *(volatile unsigned char*)0x2B |= (1<<2 | 1<<3);
 
-    //EIMSK(0x3d): INT(1:0): enable;
-    *(volatile unsigned char*)0x3d = 0b00000011;
+        // PORTD(0x2B): ( PD2: PD3: ) <- using internal PULL_UP(1):
+    *(volatile unsigned char*)0x2B |= (1<<2 | 1<<3);;
+
+    //EIMSK(0x3d): INT(1~0): enable;
+    *(volatile unsigned char*)0x3d |= (1<<0 | 1<<1);
     //EICRA(0x69): rising edge(11) sensor ;
-    *(volatile unsigned char*)0x69 = 0b00001010; // EICRA(0x69): rising edge(1:1)
+    *(volatile unsigned char*)0x69 |= 0b00001111; // EICRA(0x69): rising edge(1:1)
 
     //sei(); SREG(7): set I(7) enable
     *(volatile unsigned char*)0x5f |= (1<<7); // SREG(0x5f):
 
     while(1){
-        // PORTB: ( PB0: PB1: PB2: PB3: ) <- LOW(0):
-        *(volatile unsigned char*)0x25 &= ~(1<<0 | 1<<1 | 1<<2 | 1<<3);    // PORTB(0x25): low
+        for(int i=1; i<0b00010000; i=i<<1){
+            for(unsigned long d=0;d<0xfffff;d++){
+                *(volatile unsigned char*)0x25 = i;
+                for(unsigned long d=0;d<0xfffff;d++);
+            }
+        }
     }
 }
+/*
+void __vector_1(void) __attribute__ ((__signal__, __used__, __externally_visible__));
+void __vector_1(void){
+    *(volatile unsigned char*)0x25 = 1;
+    for(unsigned long d=0;d<0xfffff;d++);
+}
+void __vector_2(void) __attribute__ ((__signal__, __used__, __externally_visible__));
+void __vector_2(void){
+    *(volatile unsigned char*)0x25 = 2;
+    for(unsigned long d=0;d<0xfffff;d++);
+}
+/*
+ISR(INT0_vect){
+    *(volatile unsigned char*)0x25 = 1;
+    for(unsigned long d=0;d<0xfffff;d++);
+}
+ISR(INT1_vect){
+    *(volatile unsigned char*)0x25 = 2;
+    for(unsigned long d=0;d<0xfffff;d++);
+}
+*/
--- avr/atmega328p/src/PortB.c
+++ avr/atmega328p/src/PortB.c
@@ -2,12 +2,10 @@
 int main(){
 
     // PB(0x23): DDRB(0x24): PB1 <- output 
-    *(volatile char*)0x24 |= (1<<1);
+    *(volatile char*)0x24 |= 0x00001111;
 
     for(;1;){
         // PB(0x23): PORTB(0x25): PB0 <- high
-        *(volatile char*)0x25 |= (1<<1);
-        // PB(0x23): PORTB(0x25): PB2 <- high
-        *(volatile char*)0x25 |= (1<<3);
+        *(volatile char*)0x25 |= 0b00001111;
     }
 }
