14df5540303b4f248a1a59b23cf2f557385df76b
karng87 <karng87@gmail.com>
2022-10-21 02:52:57 +0900

    asus

--- avr/atmega328p/src/EICRA.c
+++ avr/atmega328p/src/EICRA.c
@@ -2,46 +2,34 @@
 
 int main(){
         // DDRB: 
-    *(volatile unsigned char*)0x24 |= 0b00001111;
+    *(volatile unsigned char*)0x24 |= 0b00111111;
         // PORTB:
-    *(volatile unsigned char*)0x25 = 0b00001111;
+    *(volatile unsigned char*)0x25 = 0b00110101;
 
         // DDRD: PD2(INT0 PIN): and PD3(INT1 PIN): <- INPUT:
-    *(volatile unsigned char*)0x2A &= 0b11111001;
+    *(volatile unsigned char*)0x2A &= 0b11110011;
         // PROTD: <- PULL_UP(1):
-    *(volatile unsigned char*)0x2B |= 0b00000110;
+    *(volatile unsigned char*)0x2B |= 0b00001100;
 
         //EIMSK(0x3d): <- MASK: [- - - - - - INT1: INT0: ]
     *(volatile unsigned char*)0x3d |= 0b00000011;
         //EICRA(0x69): [ - - - - ISC11: ISC10: ISC01: ISC00: ]
     *(volatile unsigned char*)0x69 |= 0b00001010; // falling edgge(10)
 
-    //sei(); SREG(7): set I(7) enable
     __asm__ __volatile__ ("sei" ::: "memory");
     //*(volatile unsigned char*)0x5f |= (1<<7); // SREG(0x5f):
-    volatile unsigned char data = 0b00000011;
-    const unsigned char addr = 0x25;
-
 
     while(1);
 }
 
 void __vector_1(void) __attribute__ ((__signal__, __used__, __externally_visible__)) __attribute__ ((__interrupt__));
 void __vector_1(void){
-    //__asm__ __volatile__ ("sei" ::: "memory");
-    //*(volatile unsigned char*)0x25 ^= 0b00000011;
-    asm volatile(
-        "push r16;"
-        "mov r16, %0;"
-        "sts 0x25,r16;"
-        "pop r16"
-        ::"I"(3):"memory"
-        );
+    *(volatile unsigned char*)0x25 ^= 0b00111111;
 }
 
 void __vector_2(void) __attribute__ ((__signal__, __used__, __externally_visible__)) __attribute__ ((__interrupt__));
 void __vector_2(void){
-    *(volatile unsigned char*)0x25 ^= 0b00001100;
+    *(volatile unsigned char*)0x25 ^= 0b00111111;
 }
 
 
--- avr/atmega328p/src/asm_interrupt.asm
+++ avr/atmega328p/src/asm_interrupt.asm
@@ -1,5 +1,79 @@
-;.equ SPH, 0x3E ; Stack Poniter High 
-;.equ SPL, 0x3D ; Stack Pointer Low
+/*
+
+ROM:
+    Flash Memory(32K:0x7d00): page 단위로만 쓰고 지울 수 있다.
+        - all program instruction(per page) are 16-bits or 32-bits
+        - 0x3fff = 16383 = 16 * 1000
+
+        +---------------------------------------------+
+        | 0x0000 ~       | Application Flash Section  |
+        |       ~ 0x3fff | Boot Flash Section         |
+        +---------------------------------------------+
+
+        - boot code section with independent lock bits
+
+    EEPROM(1K:0x400): byte 단위로 쓰고 지울수 있다.
+        program data(per byte)
+        
+RAM:
+    DRAM: for data 
+    SRAM: for register
+        - 2K(0x1000) - internal SDRAM
+       +------------------------------------------------+
+       | 0x0000 ~ 0x001f | 32(0x20)  Register           |
+       | 0x0020 ~ 0x005f | 64(0x40)  I/O register       |
+       | 0x0060 ~ 0x00ff | 160(0xA0) Ext I/O register   |
+       | -----------------------------------------------
+       | 0x0100 ~ 0x08ff | internal SRAM(2048-0x7ff)    |
+       +------------------------------------------------+
+
+       - All I/O location
+            - LD/LDS/LDD, ST/STS/STD  with 32 general purpose registers
+        0x00 ~ 0x1f: SBI, SBIC,SBIS(Skip BIt Set, CBI(Clear BIt)
+                    only used on register containing such status flags
+
+        0x00 ~ 0x3f: IN OUT <- When using the I/O specific commands
+                     LD ST <-  When addressing I/O registers as DATA space, 0x20 must be added  
+        0x60 ~ 0xff: LD/LDS/LDD, ST/STS/STD can be used only.
+5 memory addressing mode
+    1. Direct
+    2. Indirect 
+    3. Indirect with displacement
+        - base address: Y,Z register
+    4. Indirect with pre-decrement
+        - X,Y,Z register
+    5. Indirect with post-increment
+        - X,Y,Z register
+    - R26:R27 R28:R29 R30:R31
+        - indirect addressing pointer registers
+    
+Memory-Mapped I/O
+    - Memory and I/O share the same address space
+    - I/O reads and writes are done in the same way as memory instruction
+*/
+;; GPRF: General Purpose Register File
+       ; R0[0x00] ~ R31[0x1F]
+       ; X: R26:R27
+       ; Y: R28:R29
+       ; Z: R30:R31
+
+.equ SPH, 0x3e  ; Stack Poniter High 
+.equ SPL, 0x3d  ; Stack Pointer Low
+.equ SREG, 0x3f ; [ I, T, H, S, V, N, Z, C ] (0x00~0x3f):IN, OUT
+
+.equ PINB, 0x3  ; 
+.equ DDRB, 0x4  ;
+.equ PORTB, 0x5 ;
+
+.equ PINC, 0x6  ; 
+.equ DDRC, 0x7  ;
+.equ PORTC, 0x8 ;
+
+.equ PIND, 0x9  ; 
+.equ DDRD, 0xa  ;
+.equ PORTD, 0xb  ;
+.equ EICRA, 0x69 ; MEMORY MAPPED
+.equ EIMSK, 0x1d
 
 .section .text
 
@@ -9,10 +83,6 @@
 .ORG 0x02
     rjmp ISR_EXT_INT0
 
-.ORG 0x04
-    rjmp ISR_EXT_INT1
-
-.ORG 0x100
 ISR_RESET:
     ldi r16, 0x08 ; SRAM DATA MEM: 
                     ; 0x00 ~ 0x1F <- 32 registers
@@ -20,43 +90,51 @@ ISR_RESET:
                     ; 0x60 ~ 0xff <- 160 Ext I/O registers
                     ; 0x60 ~ 0x08ff <- 1018 * 8 internal SRAM
     
-    out 0x3e, r16    ; str SPH+0x20, r 16
+    out SPH, r16    ; str SPH+0x20, r 16
     ldi r16, 0xff   ; 0x08ff
-    out 0x3d, r16
+    out SPL, r16
     ; SET SP
 
-    ldi r16, 0b11111111 ; PD5 (0x23)
-    sts 0x24, r16 ; DDRD5
-    sts 0x25, r16 ; PORTD5
-
-    ldi r16, 0b11111011     ; PD2 0x29
-    sts 0x2A, r16           ; DDRD2: INPUT
-    ldi r16, 0b00000100     ; PORTD2: high
-    sts 0x2B, r16
-
-    ldi r16, 0b00000001
-    lds r17, 0x3d           ; EIMSK : [--|INT1|INT0]
+    ldi r16, 0b00111111 ; PB5 (0x23)
+    IN r17, DDRB 
     or r16, r17
-    sts 0x3d, r16
+    out DDRB, r16 ; DDRB5
 
-    ldi r16, 0b00000010
-    lds r17, 0x69           ; EICRA : [[--|ISC01|ISC00] faaling edge
+    ldi r16, 0b00110011 ; PB5 (0x23)
+    IN r17, PORTB ; PORTD5
     or r16, r17
-    sts 0x69, r16
+    OUT PORTB, r16
 
-    sei
+    ldi r16, 0b11111011  ; PD2 set input mode
+    IN r17, DDRD
+    and r16, r17
+    OUT DDRD, r16
+
+
+
+    ldi r16, 0b00000100     ; DDRD2: INPUT set pull up
+    IN r17, PORTD; PORTD2: high
+    or r16, r17
+    OUT PORTD, r16
+
+    ldi r16, 0b00000010     ; [--- ISC01:ISC00] <- 10 :falling edge in INT0
+    LDS r17, EICRA
+    or r16, r17
+    STS EICRA, r16
+
+    ldi r16, 0b00000001     ; [--- INT0] <- 1
+    LDS r17, EIMSK          ; EIMSK : [--|INT1|INT0]
+    or r16, r17
+    STS EIMSK, r16
+
+    SEI                 ; global interrupt enable
 
 L:
-    rjmp L
+    jmp L
 
-.ORG 0x200
 ISR_EXT_INT0:
-    lds r21, 0x25
-    lds r22, 0b00100000
-    eor r21, r22
-    sts 0x25, r21
-    reti
-
-.ORG 0x300
-ISR_EXT_INT1:
+    IN r16, PORTB  
+    LDI r17, 0b00111111
+    EOR r16, r17
+    OUT PORTB, r16
     reti
