15bb3386b97cf6a358056cd3a55e80a1131a4ce1
karng87 <karng87@gmail.com>
2022-11-04 19:44:18 +0900

    pico-boot customize 01

--- pico/include/boards/pico.h
+++ pico/include/boards/pico.h
@@ -19,47 +19,50 @@
 
 // --- UART ---
 #ifndef PICO_DEFAULT_UART
-#define PICO_DEFAULT_UART 0
+    #define PICO_DEFAULT_UART 0
 #endif
+
 #ifndef PICO_DEFAULT_UART_TX_PIN
-#define PICO_DEFAULT_UART_TX_PIN 0
+    #define PICO_DEFAULT_UART_TX_PIN 0
 #endif
+
 #ifndef PICO_DEFAULT_UART_RX_PIN
-#define PICO_DEFAULT_UART_RX_PIN 1
+    #define PICO_DEFAULT_UART_RX_PIN 1
 #endif
 
 // --- LED ---
 #ifndef PICO_DEFAULT_LED_PIN
-#define PICO_DEFAULT_LED_PIN 25
+    #define PICO_DEFAULT_LED_PIN 25
 #endif
 // no PICO_DEFAULT_WS2812_PIN
 
 // --- I2C ---
 #ifndef PICO_DEFAULT_I2C
-#define PICO_DEFAULT_I2C 0
+    #define PICO_DEFAULT_I2C 0
 #endif
+
 #ifndef PICO_DEFAULT_I2C_SDA_PIN
-#define PICO_DEFAULT_I2C_SDA_PIN 4
+    #define PICO_DEFAULT_I2C_SDA_PIN 4
 #endif
 #ifndef PICO_DEFAULT_I2C_SCL_PIN
-#define PICO_DEFAULT_I2C_SCL_PIN 5
+    #define PICO_DEFAULT_I2C_SCL_PIN 5
 #endif
 
 // --- SPI ---
 #ifndef PICO_DEFAULT_SPI
-#define PICO_DEFAULT_SPI 0
+    #define PICO_DEFAULT_SPI 0
 #endif
 #ifndef PICO_DEFAULT_SPI_SCK_PIN
-#define PICO_DEFAULT_SPI_SCK_PIN 18
+    #define PICO_DEFAULT_SPI_SCK_PIN 18
 #endif
 #ifndef PICO_DEFAULT_SPI_TX_PIN
 #define PICO_DEFAULT_SPI_TX_PIN 19
 #endif
 #ifndef PICO_DEFAULT_SPI_RX_PIN
-#define PICO_DEFAULT_SPI_RX_PIN 16
+    #define PICO_DEFAULT_SPI_RX_PIN 16
 #endif
 #ifndef PICO_DEFAULT_SPI_CSN_PIN
-#define PICO_DEFAULT_SPI_CSN_PIN 17
+    #define PICO_DEFAULT_SPI_CSN_PIN 17
 #endif
 
 // --- FLASH ---
@@ -67,18 +70,18 @@
 #define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
 
 #ifndef PICO_FLASH_SPI_CLKDIV
-#define PICO_FLASH_SPI_CLKDIV 2
+    #define PICO_FLASH_SPI_CLKDIV 2
 #endif
 
 #ifndef PICO_FLASH_SIZE_BYTES
-#define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
+    #define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
 #endif
 
 // Drive high to force power supply into PWM mode (lower ripple on 3V3 at light loads)
-#define PICO_SMPS_MODE_PIN 23
+#define PICO_SMPS_MODE_PIN 23 // SMPS: Switching Mode Power Supply
 
 #ifndef PICO_RP2040_B0_SUPPORTED
-#define PICO_RP2040_B0_SUPPORTED 1
+    #define PICO_RP2040_B0_SUPPORTED 1
 #endif
 
 #endif
--- pico/include/pico/config_autogen.h
+++ pico/include/pico/config_autogen.h
@@ -3,9 +3,124 @@
 
 
 // based on PICO_CONFIG_HEADER_FILES:
+#ifndef _BOARDS_PICO_H
+    #define _BOARDS_PICO_H
 
-#include "/Users/jkarng/Project/rasp/pico-sdk/src/boards/include/boards/pico.h"
+// For board detection
+    #define RASPBERRYPI_PICO
 
+// --- UART ---
+    #ifndef PICO_DEFAULT_UART
+        #define PICO_DEFAULT_UART 0
+    #endif
+    #ifndef PICO_DEFAULT_UART_TX_PIN
+        #define PICO_DEFAULT_UART_TX_PIN 0
+    #endif
+    #ifndef PICO_DEFAULT_UART_RX_PIN
+        #define PICO_DEFAULT_UART_RX_PIN 1
+    #endif
+
+// --- LED ---
+    #ifndef PICO_DEFAULT_LED_PIN
+        #define PICO_DEFAULT_LED_PIN 25
+    #endif
+// no PICO_DEFAULT_WS2812_PIN
+
+// --- I2C ---
+    #ifndef PICO_DEFAULT_I2C
+        #define PICO_DEFAULT_I2C 0
+    #endif
+    #ifndef PICO_DEFAULT_I2C_SDA_PIN
+        #define PICO_DEFAULT_I2C_SDA_PIN 4
+    #endif
+    #ifndef PICO_DEFAULT_I2C_SCL_PIN
+        #define PICO_DEFAULT_I2C_SCL_PIN 5
+    #endif
+
+// --- SPI ---
+    #ifndef PICO_DEFAULT_SPI
+        #define PICO_DEFAULT_SPI 0
+    #endif
+    #ifndef PICO_DEFAULT_SPI_SCK_PIN
+        #define PICO_DEFAULT_SPI_SCK_PIN 18
+    #endif
+    #ifndef PICO_DEFAULT_SPI_TX_PIN
+        #define PICO_DEFAULT_SPI_TX_PIN 19
+    #endif
+    #ifndef PICO_DEFAULT_SPI_RX_PIN
+        #define PICO_DEFAULT_SPI_RX_PIN 16
+    #endif
+    #ifndef PICO_DEFAULT_SPI_CSN_PIN
+        #define PICO_DEFAULT_SPI_CSN_PIN 17
+    #endif
+
+// --- FLASH ---
+
+    #define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+    #ifndef PICO_FLASH_SPI_CLKDIV
+        #define PICO_FLASH_SPI_CLKDIV 2
+    #endif
+
+    #ifndef PICO_FLASH_SIZE_BYTES
+        #define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
+    #endif
+
+// Drive high to force power supply into PWM mode (lower ripple on 3V3 at light loads)
+    #define PICO_SMPS_MODE_PIN 23
+
+    #ifndef PICO_RP2040_B0_SUPPORTED
+        #define PICO_RP2040_B0_SUPPORTED 1
+    #endif
+
+#endif
+
+//------------------------------------------
 // based on PICO_RP2040_CONFIG_HEADER_FILES:
+//------------------------------------------
+#ifndef _CMSIS_RENAME_EXCEPTIONS_H
+    #define _CMSIS_RENAME_EXCEPTIONS_H
 
-#include "/Users/jkarng/Project/rasp/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h"
+    #if LIB_CMSIS_CORE
+        // PICO_CONFIG: PICO_CMSIS_RENAME_EXCEPTIONS, Whether to rename SDK exceptions such as isr_nmi to their CMSIS equivalent i.e. NMI_Handler, type=bool, default=1, group=cmsis_core
+
+        // Note that since this header is included at the config stage, if you wish to override this you should do so via build compiler define
+        #ifndef PICO_CMSIS_RENAME_EXCEPTIONS
+            #define PICO_CMSIS_RENAME_EXCEPTIONS 1
+        #endif
+
+        #if PICO_CMSIS_RENAME_EXCEPTIONS
+            #define isr_nmi NMI_Handler
+            #define isr_hardfault HardFault_Handler
+            #define isr_svcall SVC_Handler
+            #define isr_pendsv PendSV_Handler
+            #define isr_systick SysTick_Handler
+            #define isr_irq0 TIMER_IRQ_0_Handler
+            #define isr_irq1 TIMER_IRQ_1_Handler
+            #define isr_irq2 TIMER_IRQ_2_Handler
+            #define isr_irq3 TIMER_IRQ_3_Handler
+            #define isr_irq4 PWM_IRQ_WRAP_Handler
+            #define isr_irq5 USBCTRL_IRQ_Handler
+            #define isr_irq6 XIP_IRQ_Handler
+            #define isr_irq7 PIO0_IRQ_0_Handler
+            #define isr_irq8 PIO0_IRQ_1_Handler
+            #define isr_irq9 PIO1_IRQ_0_Handler
+            #define isr_irq10 PIO1_IRQ_1_Handler
+            #define isr_irq11 DMA_IRQ_0_Handler
+            #define isr_irq12 DMA_IRQ_1_Handler
+            #define isr_irq13 IO_IRQ_BANK0_Handler
+            #define isr_irq14 IO_IRQ_QSPI_Handler
+            #define isr_irq15 SIO_IRQ_PROC0_Handler
+            #define isr_irq16 SIO_IRQ_PROC1_Handler
+            #define isr_irq17 CLOCKS_IRQ_Handler
+            #define isr_irq18 SPI0_IRQ_Handler
+            #define isr_irq19 SPI1_IRQ_Handler
+            #define isr_irq20 UART0_IRQ_Handler
+            #define isr_irq21 UART1_IRQ_Handler
+            #define isr_irq22 ADC_IRQ_FIFO_Handler
+            #define isr_irq23 I2C0_IRQ_Handler
+            #define isr_irq24 I2C1_IRQ_Handler
+            #define isr_irq25 RTC_IRQ_Handler
+        #endif
+    #endif
+#endif /* _CMSIS_RENAME_EXCEPTIONS_H */
