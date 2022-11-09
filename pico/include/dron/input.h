d98839733e204a395ac551b3cf9c3de277e908c7
karng87 <karng87@gmail.com>
2022-11-09 19:49:36 +0900

    imac

---
+++ pico/include/dron/input.h
@@ -0,0 +1,13 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+void input_proc(
+                char type, 
+                char* p_usb_char, 
+                char* p_usb_string, 
+                const int* p_USB_STRING_SIZE);
+
+void flush_input(
+                char* p_usb_string);
+
+#endif
---
+++ pico/include/dron/print.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+void print_proc(
+                char* p_usb_char,
+                char* p_usb_string);
+
+#endif
