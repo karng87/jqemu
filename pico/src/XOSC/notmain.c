a135460bcc124da67cef185b9271bb85974abee6
karng87 <karng87@gmail.com>
2022-11-30 09:07:44 +0900

    Linux

--- pico/src/XOSC/notmain.c
+++ pico/src/XOSC/notmain.c
@@ -1,134 +1,157 @@
-void PUT32(unsigned int, unsigned int);
-unsigned int GET32(unsigned int);
+void PUT32 ( unsigned int, unsigned int );
+unsigned int GET32 ( unsigned int );
 
-        #define XOR 0x1000
-        #define SET 0x2000
-        #define CLR 0x3000
+#define RESETS_BASE                 0x4000C000
 
-#define hex(x,y) (0x ## x ## y)
-#define PPB_BASE hex(E000,0000) // Pirvate Peripheral Bus
-        // [31:17| 16:<COUNTFLAG> | 15:3 | 2:<CLKSOURCE> | 1:<TICKINT> | 0:<ENABLE> ]
-    #define SYST_CSR  0xE010 // SysTic Control and Status Register
-        #define flag_count_flag 16 // return 0 if timer counted to 0
-        #define flag_systic_count_enable 0 // system tick counter enable
-            #define bit_counter_disable 0
-            #define bit_counter_enable 1
-        #define flag_interrupt 1
-        #define flag_clksource 2
-            #define bit_external_reference_clock 0
-            #define bit_processor_clock 1
-    #define SYST_RVR  0xE014 // SysTic Reload Value Register 초기값 설정
-        #define flag_reload_23 0
-    #define SYST_CVR  0xE018 // SysTic Current Value Register
-        #define flag_current_23 0
-    #define SYST_CALB 0xe01c // SysTic Callibration Value Register
+#define RESETS_RESET_RW        (RESETS_BASE+0x0+0x0000)
+#define RESETS_RESET_XOR       (RESETS_BASE+0x0+0x1000)
+#define RESETS_RESET_SET       (RESETS_BASE+0x0+0x2000)
+#define RESETS_RESET_CLR       (RESETS_BASE+0x0+0x3000)
+
+#define RESETS_WDSEL_RW        (RESETS_BASE+0x4+0x0000)
+#define RESETS_WDSEL_XOR       (RESETS_BASE+0x4+0x1000)
+#define RESETS_WDSEL_SET       (RESETS_BASE+0x4+0x2000)
+#define RESETS_WDSEL_CLR       (RESETS_BASE+0x4+0x3000)
+
+#define RESETS_RESET_DONE_RW   (RESETS_BASE+0x8+0x0000)
+#define RESETS_RESET_DONE_XOR  (RESETS_BASE+0x8+0x1000)
+#define RESETS_RESET_DONE_SET  (RESETS_BASE+0x8+0x2000)
+#define RESETS_RESET_DONE_CLR  (RESETS_BASE+0x8+0x3000)
+
+#define SIO_BASE                    0xD0000000
+
+#define SIO_GPIO_OUT_RW             (SIO_BASE+0x10)
+#define SIO_GPIO_OUT_SET            (SIO_BASE+0x14)
+#define SIO_GPIO_OUT_CLR            (SIO_BASE+0x18)
+#define SIO_GPIO_OUT_XOR            (SIO_BASE+0x1C)
+
+#define SIO_GPIO_OE_RW              (SIO_BASE+0x20)
+#define SIO_GPIO_OE_SET             (SIO_BASE+0x24)
+#define SIO_GPIO_OE_CLR             (SIO_BASE+0x28)
+#define SIO_GPIO_OE_XOR             (SIO_BASE+0x2C)
+
+#define IO_BANK0_BASE               0x40014000
+
+#define IO_BANK0_GPIO25_STATUS_RW   (IO_BANK0_BASE+0x0C8+0x0000)
+#define IO_BANK0_GPIO25_STATUS_XOR  (IO_BANK0_BASE+0x0C8+0x1000)
+#define IO_BANK0_GPIO25_STATUS_SET  (IO_BANK0_BASE+0x0C8+0x2000)
+#define IO_BANK0_GPIO25_STATUS_CLR  (IO_BANK0_BASE+0x0C8+0x3000)
+
+#define IO_BANK0_GPIO25_CTRL_RW     (IO_BANK0_BASE+0x0CC+0x0000)
+#define IO_BANK0_GPIO25_CTRL_XOR    (IO_BANK0_BASE+0x0CC+0x1000)
+#define IO_BANK0_GPIO25_CTRL_SET    (IO_BANK0_BASE+0x0CC+0x2000)
+#define IO_BANK0_GPIO25_CTRL_CLR    (IO_BANK0_BASE+0x0CC+0x3000)
 
 
-static void do_delay(unsigned int x){
-    for( unsigned int i=0;i<x;){
-        if(GET32(PPB_BASE + SYST_CSR) & (1<< flag_count_flag)) i++; 
+#define CLOCKS_BASE                 0x40008000
+
+
+#define CLK_REF_CTRL_RW             (CLOCKS_BASE+0x30+0x0000)
+#define CLK_REF_CTRL_XOR            (CLOCKS_BASE+0x30+0x1000)
+#define CLK_REF_CTRL_SET            (CLOCKS_BASE+0x30+0x2000)
+#define CLK_REF_CTRL_CLR            (CLOCKS_BASE+0x30+0x3000)
+
+#define CLK_SYS_CTRL_RW             (CLOCKS_BASE+0x3C+0x0000)
+#define CLK_SYS_CTRL_XOR            (CLOCKS_BASE+0x3C+0x1000)
+#define CLK_SYS_CTRL_SET            (CLOCKS_BASE+0x3C+0x2000)
+#define CLK_SYS_CTRL_CLR            (CLOCKS_BASE+0x3C+0x3000)
+
+
+
+#define CLK_SYS_RESUS_CTRL_RW       (CLOCKS_BASE+0x78+0x0000)
+#define CLK_SYS_RESUS_CTRL_XOR      (CLOCKS_BASE+0x78+0x1000)
+#define CLK_SYS_RESUS_CTRL_SET      (CLOCKS_BASE+0x78+0x2000)
+#define CLK_SYS_RESUS_CTRL_CLR      (CLOCKS_BASE+0x78+0x3000)
+
+
+#define XOSC_BASE                   0x40024000
+
+#define XOSC_CTRL_RW                (XOSC_BASE+0x00+0x0000)
+#define XOSC_CTRL_XOR               (XOSC_BASE+0x00+0x1000)
+#define XOSC_CTRL_SET               (XOSC_BASE+0x00+0x2000)
+#define XOSC_CTRL_CLR               (XOSC_BASE+0x00+0x3000)
+
+#define XOSC_STATUS_RW              (XOSC_BASE+0x04+0x0000)
+#define XOSC_STATUS_XOR             (XOSC_BASE+0x04+0x1000)
+#define XOSC_STATUS_SET             (XOSC_BASE+0x04+0x2000)
+#define XOSC_STATUS_CLR             (XOSC_BASE+0x04+0x3000)
+
+#define XOSC_STARTUP_RW             (XOSC_BASE+0x0C+0x0000)
+#define XOSC_STARTUP_XOR            (XOSC_BASE+0x0C+0x1000)
+#define XOSC_STARTUP_SET            (XOSC_BASE+0x0C+0x2000)
+#define XOSC_STARTUP_CLR            (XOSC_BASE+0x0C+0x3000)
+
+#define STK_CSR 0xE000E010
+#define STK_RVR 0xE000E014
+#define STK_CVR 0xE000E018
+
+static void do_delay ( unsigned int x )
+{
+    unsigned int ra;
+
+    for(ra=0;ra<x;)
+    {
+        if((GET32(STK_CSR)&(1<<16))!=0)
+        {
+            ra++;
+        }
     }
 }
-
-static void clock_init(){
-#define CLOCKS hex(4000,8000)
-    #define REF_CTRL 0x30 // [ 6:5<AUXSRC> | 1:0<SRC> ]
-        #define flag_src_1 0
-            #define bit_rosc_clksrc_ph 0
-            #define bit_clksrc_clk_ref_aux 1
-            #define bit_xosc_clksrc 2
-        
-    #define SYS_CTRL 0x38 // [31:7 | 6:5<AUXSRC> | 4:2 | 1:0<SRC> ]
-        #define flag_src 0
-            #define bit_clk_ref 0
-            #define bit_clksrc_clk_sys_aux 1
-    #define SYS_RESUS_CTRL 0x78 //[31:17 | 16<CLEAR> | 15:13 | 12:<FRCE:force a resus> | 11:9 | 8:<ENABLE> 7:0<TIEOUT>]
-    PUT32(CLOCKS + SYS_RESUS_CTRL,0);
-
-#define XOSC hex(4002,4000) // eXternal OSCillator
-    #define CTRL    0x00 // [23:12(12-0x_zzz<ENABLE> | 11:0(12-0x_zzz)<FREQ_RANGE>]
-        #define flag_enable_23 12
-            #define enable_bit 0xfab
-            #define disable_bit 0xd1e
-        #define flag_freq_range_11 0
-            #define bit_1_15mhz 0xaa0
-    #define STATUS  0x04 // [31:<STABLE> | 24:<BADWRITE> | 12:<ENABLED> | 1:0<FREQ_RANGE>]
-        #define flag_stable 31
-        //#define freq_range_flag 0
-            #define bit_status_1_15mhz 0
-    #define DORMANT 0x08 // 휴지상태의 잠든상태의
-    #define STARTUP 0x0c //[20:<X4> | 13:0<DELAY>]
-        #define delay_flag 0
-    #define COUNT   0x1c
-    PUT32((XOSC + CTRL),(bit_1_15mhz << flag_freq_range_11));
-    PUT32((XOSC + STARTUP),(47 << delay_flag)); // straight from the datasheet
-    PUT32((XOSC + CTRL + SET),(enable_bit << flag_enable_23));
-    while(1){
-        if(GET32(XOSC + STATUS) & (1<<flag_stable)) break;
+static void clock_init ( void )
+{
+    PUT32(CLK_SYS_RESUS_CTRL_RW,0);
+    //PUT32(CLK_REF_CTRL_RW,0);
+    //PUT32(CLK_SYS_CTRL_RW,0);
+    PUT32(XOSC_CTRL_RW,0xAA0);      //1 - 15MHZ
+    PUT32(XOSC_STARTUP_RW,47);      //straight from the datasheet
+    PUT32(XOSC_CTRL_SET,0xFAB000);  //enable
+    while(1)
+    {
+        if((GET32(XOSC_STATUS_RW)&0x80000000)!=0) break;
     }
-    PUT32((CLOCKS + REF_CTRL),(bit_xosc_clksrc << flag_src_1));
-    PUT32((CLOCKS + SYS_CTRL),(bit_clk_ref << flag_src));
+    PUT32(CLK_REF_CTRL_RW,2); //XOSC
+    PUT32(CLK_SYS_CTRL_RW,0); //reset/clk_ref
 }
 
-int notmain(){
+int notmain ( void )
+{
+    unsigned int ra;
+
     clock_init();
 
-#define RESETS hex(4000,c000)
-    #define RESET (0x0) // [24<usbctrl>|23<uart1>|22<uart0>|21<timer>|20<tbman>|5<io_bank0]
-        #define flag_io_bank0 5
-    #define WDSEL (0x4)
-    #define DONE  (0x8)
-        //#define flag_io_bank0 5
-    PUT32((RESETS+RESET+CLR),(1 << flag_io_bank0));
-    while(1){
-        if(GET32(RESETS + DONE) & 1<<flag_io_bank0) break;
+    PUT32(RESETS_RESET_CLR,1<<5); //IO_BANK0
+    while(1)
+    {
+        if((GET32(RESETS_RESET_DONE_RW)&(1<<5))!=0) break;
     }
-#define SIO hex(D000,0000)
-            #define pin25 25
-        #define OUT_SET 0x14
-        #define OUT_CLR 0x18
-        #define OUT_XOR 0x1c
 
-        #define OE_SET 0x24
-        #define OE_CLR 0x28
-        #define OE_XOR 0x2c
+    PUT32(SIO_GPIO_OE_CLR,1<<25);
+    PUT32(SIO_GPIO_OUT_CLR,1<<25);
+    PUT32(IO_BANK0_GPIO25_CTRL_RW,5); //SIO
+    PUT32(SIO_GPIO_OE_SET,1<<25);
 
-#define IO_BANK0 hex(4001,4000)
-    #define CTRL_IO_25 0xcc // [4:0<funsel>]
-        #define flag_funcsel_4 0
-            #define bit_sio_func 5
-#define PADS_BNAK0 hex(4001,c000)
-    #define CTRL_PADS_25 0x68 // [7:<OD>|6:<IE>]
-        #define flag_ie 6
-        #define flag_od 7
-    PUT32((SIO + OE_CLR),(1<<pin25));
-    PUT32((SIO + OUT_CLR),(1<<pin25));
-    PUT32((IO_BANK0 + CTRL_IO_25),(bit_sio_func << flag_funcsel_4));
-    PUT32((SIO + OE_SET),(1<<pin25));
-/*
-    #define PPB_BASE hex(e000,0000) // Processor Peripheral Bus
-            // [31:17| 16:<COUNTFLAG> | 15:3 | 2:<CLKSOURCE> | 1:<TICKINT> | 0:<ENABLE> ]
-        #define SYST_CSR  0xE010 // SysTic Control and Status Register
-            #define flag_count 16 // return 0 if timer counted to 0
-            #define flag_clksource 2
-                #define bit_external_reference_clock 0
-                #define bit_processor_clock 1
-        #define SYST_RVR  0xe014 // SysTic Reload Value Register [23:0<reload>]
-*/
-            #define flag_reload_23 0
-/*
-            reload value = N clock cyles - 1
-        #define SYST_CVR  0xe018 // SysTic Current Value Register
-        #define SYST_CALB 0xe01c // SysTic Callibration Value Register
-*/
-    PUT32((PPB_BASE + SYST_CSR),(bit_processor_clock << flag_clksource));
-    PUT32((PPB_BASE + SYST_RVR),(12000000-1)); // 12Mhz
-    PUT32((PPB_BASE + SYST_CVR),(12000000-1)); // 12Mhz
-    PUT32((PPB_BASE + SYST_CSR),(bit_processor_clock<<flag_clksource | bit_counter_enable<<flag_systic_count_enable)); // 12Mhz
-    
-    for(unsigned int i=0;i<100;i++){
-        PUT32(SIO+OUT_XOR,1<<pin25);
-        do_delay(10);
+    PUT32(STK_CSR,0x00000004);
+    PUT32(STK_RVR,12000000-1);
+    PUT32(STK_CVR,12000000-1);
+    PUT32(STK_CSR,0x00000005);
+
+    for(ra=0;ra<100;ra++)
+    {
+        PUT32(SIO_GPIO_OUT_XOR,1<<25);
+        do_delay(1);
     }
-    return 0;
+
+    return(0);
 }
+
+//-------------------------------------------------------------------------
+//
+// Copyright (c) 2021 David Welch dwelch@dwelch.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------
--- pico/src/XOSC/start.s
+++ pico/src/XOSC/start.s
@@ -1,13 +1,14 @@
-.cpu cortex-m0plus
+
+.cpu cortex-m0
 .thumb
 
-.global vector_table
+.globl vector_table
 vector_table:
 .thumb_func
-.global centry
+.globl centry
 centry:
     b reset
-    .balign 4 ;@ b:byte align 4 byte align
+    .balign 4
     .word reset ;@ has to be offset 4
     .word loop
     .word loop
@@ -30,32 +31,46 @@ centry:
 
 .thumb_func
 reset:
-    ldr r1, =0xE000ED08 ;@ VTOR Vector Table Offset Register
-    ldr r0, =vector_table
-    str r0, [r1]
+    ldr r1,=0xE000ED08 ;@ VTOR
+    ldr r0,=vector_table
+    str r0,[r1]
 
-    ldr r0, =0x20042000 ;@ SRAM_Y end
-    mov sp, r0
+    ldr r0,=0x20002000
+    mov sp,r0
     bl notmain
     b loop
+
 .thumb_func
 loop:
     b loop
 
 .align
-.ltorg ;@ Literal pool origin == reset literal pool for psseudo-intructions(ldr vldr wldr) literal pool
+.ltorg
 
-;@---------------
-.balign 0x100 ;@ align b(byte number) cf)p2aling ( power of 2 align: 2^num align)
+
+;@ ----------------------------------
+.balign 0x100
 
 .thumb_func
-.global PUT32
+.globl PUT32
 PUT32:
     str r1,[r0]
     bx lr
 
 .thumb_func
-.global GET32
+.globl GET32
 GET32:
     ldr r0,[r0]
     bx lr
+
+;@-------------------------------------------------------------------------
+;@
+;@ Copyright (c) 2021 David Welch dwelch@dwelch.com
+;@
+;@ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+;@
+;@ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+;@
+;@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+;@
+;@-------------------------------------------------------------------------
